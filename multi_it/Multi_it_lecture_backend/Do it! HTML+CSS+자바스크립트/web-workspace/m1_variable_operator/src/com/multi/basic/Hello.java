6a8da560892180331d6769d28dd431f7d27d3919
TheBeginnerJiyeon <150709167+TheBeginnerJiyeon@users.noreply.github.com>
2024-04-16 17:17:43 +0900

    sd
    sd

---
+++ multi_it/Multi_it_lecture_backend/Do it! HTML+CSS+자바스크립트/web-workspace/m1_variable_operator/src/com/multi/basic/Hello.java
@@ -0,0 +1,19 @@
+package com.multi.basic;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+    }
+
+}
