57f821f475477f5d9b401f4b53fff796532bc4ad
TheBeginnerJiyeon <150709167+TheBeginnerJiyeon@users.noreply.github.com>
2024-04-21 05:39:53 +0900

    1
    1

--- multi_it/backend/web-workspace/practice/exam08/Button.java
+++ multi_it/backend/web-workspace/practice/exam08/Button.java
@@ -1,20 +1,22 @@
-package com.multi.practice.exam06;
+package com.multi.practice.exam08;
 
 public class Button {
+	
 	OnClickListener listener;
 	
 	void setOnClickListener(OnClickListener listener) {
 		this.listener = listener;
+		
+		
 	}
 	
-	void touch(){
+	void touch() {
 		listener.onClick();
 	}
 	
-	static interface OnClickListener{
+	static interface OnClickListener {
 		void onClick();
 	}
-
-
-
+	
+	
 }
