170f0f7138cc286ffe47fc9170c3a3832c6fa8a7
TheBeginnerJiyeon <150709167+TheBeginnerJiyeon@users.noreply.github.com>
2024-04-24 01:25:37 +0900

    1
    1

---
+++ multi_it/backend/web-workspace/m5_constructor_static/src/com/multi/practice/d_package/controller/CatalogueController.java
@@ -0,0 +1,128 @@
+package com.multi.practice.d_package.controller;
+
+import com.multi.practice.d_package.model.Product;
+
+import java.util.Scanner;
+
+public class CatalogueController {
+	
+	Scanner sc = new Scanner(System.in);
+	public static Product[] pr_list = new Product[10];
+	
+	public void insertProduct() {
+		
+		if (Product.count >= 10) {
+			System.out.println("입력 갯수 초과입니다.");
+			return;
+		}
+		
+		System.out.print("숫자 아이디를 입력: ");
+		int id = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.print("제품명을 입력: ");
+		String name = sc.nextLine();
+		
+		
+		System.out.print("가격을 입력: ");
+		int price = sc.nextInt();
+		sc.nextLine();
+		
+		
+		Product pr = new Product(id, name, price);
+		
+		pr_list[Product.count - 1] = pr;
+		
+	}
+	
+	
+	public void printProduct(String choice) {
+		
+		if (choice.equals("yes")) {
+			for (int i = 0; i < Product.count; i++) {
+				System.out.println(pr_list[i]);
+			}
+		} else {
+			System.out.println("몇번째 제품을 조회하고 싶으십니까?");
+			int item_num = sc.nextInt();
+			sc.nextLine();
+			System.out.println(pr_list[item_num - 1]);
+			
+			
+		}
+		
+		
+	}
+	
+	
+	public void updateProduct1() {
+		System.out.println("몇번째 제품을 수정하고 싶으십니까?");
+		
+		int item_num = sc.nextInt();
+		
+		Product product = pr_list[item_num - 1];
+		
+		
+		System.out.print("숫자 아이디를 입력: ");
+		int id = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.print("제품명을 입력: ");
+		String name = sc.nextLine();
+		
+		System.out.print("가격을 입력: ");
+		int price = sc.nextInt();
+		sc.nextLine();
+		
+		
+		product.setId(id);
+		product.setName(name);
+		product.setPrice(price);
+		System.out.println("제품의 정보가 변경되었습니다.");
+		System.out.println(product.toString());
+		
+		
+	}
+	
+	public void updateProduct2() {
+		
+		System.out.println("몇번째 제품을 수정하고 싶으십니까?");
+		int item_num = sc.nextInt();
+		
+		Product product = pr_list[item_num - 1];
+		
+		System.out.print("제품명을 입력: ");
+		String name = sc.next();
+		sc.nextLine();
+		
+		System.out.print("가격을 입력: ");
+		int price = sc.nextInt();
+		sc.nextLine();
+		
+		product.setName(name);
+		product.setPrice(price);
+		System.out.println("제품의 정보가 변경되었습니다.");
+		System.out.println(product.toString());
+		
+	}
+	
+	public void updateProduct3() {
+		
+		System.out.println("몇번째 제품을 수정하고 싶으십니까?");
+		int item_num = sc.nextInt();
+		
+		Product product = pr_list[item_num - 1];
+		
+		System.out.print("가격을 입력: ");
+		int price = sc.nextInt();
+		sc.nextLine();
+		
+		product.setPrice(price);
+		System.out.println("제품의 정보가 변경되었습니다.");
+		System.out.println(product.toString());
+		
+		
+	}
+	
+	
+}
---
+++ multi_it/backend/web-workspace/m5_constructor_static/src/com/multi/practice/d_package/model/Product.java
@@ -0,0 +1,59 @@
+package com.multi.practice.d_package.model;
+
+public class Product {
+	
+	
+	private int id;
+	private String name;
+	private double price;
+	public static int count = 0;
+	
+	public Product() {
+		
+		count++;
+	}
+	
+	public Product(int id, String name, double price) {
+		
+		count++;
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product{" +
+				
+				"id=" + id +
+				", name='" + name + '\'' +
+				", price=" + price +
+				'}';
+	}
+}
