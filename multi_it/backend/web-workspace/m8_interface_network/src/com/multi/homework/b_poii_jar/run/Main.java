f8e097c305e39eac60c36f13fe0ffea6693fc182
TheBeginnerJiyeon <jyu35467@gmail.com>
2024-04-30 10:50:41 +0900

    1
    1

--- multi_it/backend/web-workspace/m8_interface_network/src/com/multi/homework/b_poii_jar/run/Main.java
+++ multi_it/backend/web-workspace/m8_interface_network/src/com/multi/homework/b_poii_jar/run/Main.java
@@ -2,11 +2,17 @@ package com.multi.homework.b_poii_jar.run;
 
 import com.multi.homework.b_poii_jar.view.View;
 
+import java.io.IOException;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		View view = new View();
 		view.viewNews();
+		
+		
+		
+		
 		
 		
 	}
